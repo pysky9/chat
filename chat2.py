22a89f253cfae86d56da51fb6e6595ab6af884e7
Derek Li <derek.j.h.li@gmail.com>
2020-11-01 20:10:46 +0800

    add line history analysis

--- chat2.py
+++ chat2.py
@@ -9,23 +9,38 @@ def read_file(filename):
 # 格式轉換
 def convert(lines):
 	allen_word_count = 0
+	allen_sticker_count = 0
+	allen_image_count = 0
 	viki_word_count = 0
+	viki_sticker_count = 0
+	viki_image_count = 0
 	for line in lines:
 		s = line.split(" ")
 		time = s[0]
 		name = s[1]
 		if "Allen" in s:
-			for msg in s[2:]:
-				allen_word_count += len(msg)
+			if s[2] == "貼圖":
+				allen_sticker_count += 1
+			elif s[2] == "圖片":
+				allen_image_count += 1
+			else :
+				for msg in s[2:]:
+					allen_word_count += len(msg)
 		elif "Viki" in s:
-			for msg in s[2:]:
-				viki_word_count += len(msg)
-	if allen_word_count > viki_word_count :
-		return "Allen說比較多話"
-	elif allen_word_count < viki_word_count :
-		return "Viki話比較多"
-	else:
-		return "兩人一樣話多"
+			if s[2] == "貼圖":
+				viki_sticker_count += 1
+			elif s[2] == "圖片":
+				viki_image_count += 1
+			else:
+				for msg in s[2:]:
+					viki_word_count += len(msg)
+	print(f"Allen傳了{allen_word_count}個字")
+	print(f"Allen傳了{allen_sticker_count}個貼圖")
+	print(f"Allen傳了{allen_image_count}張圖片")
+	print(f"Viki傳了{viki_word_count}個字")
+	print(f"Viki傳了{viki_sticker_count}個貼圖")
+	print(f"Viki傳了{viki_image_count}張圖片")
+
 
 # 寫入函式
 def write_file(filename, lines):
@@ -36,6 +51,6 @@ def write_file(filename, lines):
 def main():
 	lines = read_file("LINE-Viki.txt")
 	lines = convert(lines)
-	write_file("output2.txt", lines)
+	# write_file("output2.txt", lines)
 
 main()
